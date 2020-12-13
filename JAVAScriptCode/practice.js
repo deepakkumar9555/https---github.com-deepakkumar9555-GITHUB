3e5dc4109e12c9a759d9fd5a87736bb17f2170be
DEEPAK KUMAR <deepakkumarjnv9555@gmail.com>
2020-12-14 02:27:04 +0530

    okay

---
+++ JAVAScriptCode/practice.js
@@ -0,0 +1,17 @@
+let a=10;
+    	console.log(a);
+    	let b=[1,2,3,4,5];
+    	console.log(b);
+    	console.log("Hello World");
+
+c=20;//global variable
+var d =30;//var have function scope
+let e=50;//block scope
+
+
+function fun(){
+	let a=5;
+	if(a===5){
+		let
+	}
+}   	
