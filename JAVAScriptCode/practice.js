045ebd4673eb91ab523809e5308b8ac49adbb469
DEEPAK KUMAR <deepakkumarjnv9555@gmail.com>
2020-12-15 03:20:00 +0530

    practice1

--- JAVAScriptCode/practice.js
+++ JAVAScriptCode/practice.js
@@ -14,4 +14,5 @@ function fun(){
 	if(a===5){
 		let
 	}
-}   	
+}  
+
---
+++ practice1.js
@@ -0,0 +1,16 @@
+function closeMe(){
+	//find the element
+	x=document.getElementById("demo");
+	//option 1:change the style attribute directly
+	x.style.display ="none";
+	//option 2:change the class 
+	//x.className="closed";
+}
+function openMe(){
+	//find the element
+	x=document.getElementById("demo");
+	//option 1: cange the style attribute directly
+	x.style.display="block";
+	//option 2:change the class
+	//x.className="open";
+}
