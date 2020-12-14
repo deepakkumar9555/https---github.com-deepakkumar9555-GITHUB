d682e795c6ffb1af62f5ad3aac34f47f97546e4e
DEEPAK KUMAR <deepakkumarjnv9555@gmail.com>
2020-12-15 03:27:20 +0530

    alter metod using classname

--- practice1.js
+++ practice1.js
@@ -2,15 +2,15 @@ function closeMe(){
 	//find the element
 	x=document.getElementById("demo");
 	//option 1:change the style attribute directly
-	x.style.display ="none";
+	//x.style.display ="none";
 	//option 2:change the class 
-	//x.className="closed";
+	x.className="closed";
 }
 function openMe(){
 	//find the element
 	x=document.getElementById("demo");
 	//option 1: cange the style attribute directly
-	x.style.display="block";
+	//x.style.display="block";
 	//option 2:change the class
-	//x.className="open";
+	x.className="open";
 }
